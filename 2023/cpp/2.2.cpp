8895cada232939dde8540f6d288c5167266318d2
Aditya Kumar Singh <cr7.aditya.cs@gmail.com>
2024-11-05 23:38:17 +0530

    2023 :: DAY 2

--- 2023/cpp/2.2.cpp
+++ 2023/cpp/2.2.cpp
@@ -6,12 +6,40 @@
 
 using namespace std;
 
+vector<string> extractSubsets(const string &line) {
+        auto result = vector<string>();
+        int start = 0, end = -1;
+        while((end = line.find(';', start)) != -1) {
+            result.push_back(line.substr(start, end - start));
+            start = end + 1;
+        }
+        result.push_back(line.substr(start, line.size() - start));
+        return result;
+}
+
+int gamePoint(const vector<string> & subsets) {
+    int red = 0, green = 0, blue = 0;
+    for(auto &subset : subsets) {
+        int start = 1, end = 0;
+        do {
+            end = subset.find(',', start);
+            int safe_end = end == -1 ? subset.size() : end;
+            int space_idx = subset.find(' ', start);
+            int balls = stoi(subset.substr(start, space_idx));
+            string color = subset.substr(space_idx + 1, safe_end - space_idx - 1);
+            start = end + 2;
+
+            if (color == "red") red = max(red, balls);
+            else if (color == "green") green = max(green, balls);
+            else if (color == "blue") blue  = max(blue, balls);
+        } while (end != -1);
+    }
+    return red * green * blue;
+}
 
 int main() {
     string line;
     int result = 0;
-
-    map<string, int> expectedCubes = {{"red", 12}, {"green", 13}, {"blue", 14}};
 
     while (getline(cin, line)) {
         int start = 0;
@@ -20,30 +48,8 @@ int main() {
         string data = line.substr(end + 1, line.size());
         int id = stoi(game.substr(game.find(' ') + 1, game.size()));
 
-        start = 0;
-        vector<string> subsets = vector<string>();
-        while((end = data.find(';', start)) != -1) {
-            subsets.push_back(data.substr(start, end - start));
-            start = end + 1;
-        }
-        subsets.push_back(data.substr(start, data.size() - start));
-        int red = 0, green = 0, blue = 0;
-        for (auto &subset : subsets) {
-            start = 1;
-            do {
-                end = subset.find(',', start);
-                int safe_end = end == -1 ? subset.size() : end;
-                int space_idx = subset.find(' ', start);
-                int balls = stoi(subset.substr(start, space_idx));
-                string color = subset.substr(space_idx + 1, safe_end - space_idx - 1);
-                start = end + 2;
-
-                if (color == "red") red = max(red, balls);
-                else if (color == "green") green = max(green, balls);
-                else if (color == "blue") blue  = max(blue, balls);
-            } while (end != -1);
-        }
-        result += red * green * blue;
+        vector<string> subsets = extractSubsets(data);
+        result += gamePoint(subsets);
     }
     cout << result ;
 }
