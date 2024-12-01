09edfd62dfedf30cca1ff8cd09d91a009304d7ed
Aditya Kumar Singh <cr7.aditya.cs@gmail.com>
2024-12-01 15:25:58 +0530

    2024 :: day 1

---
+++ 2024/cpp/01-1.cpp
@@ -0,0 +1,23 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  vector<int> left, right;
+  int l, r;
+  while (cin >> l >> r) {
+    left.push_back(l);
+    right.push_back(r);
+  }
+
+  sort(left.begin(), left.end());
+  sort(right.begin(), right.end());
+
+  long long result = 0;
+  for (int i = 0; i < left.size(); i++) {
+    result += abs(left[i] - right[i]);
+  }
+  cout << result << endl;
+}
---
+++ 2024/cpp/01-2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+int main() {
+  map<int, int> left, right;
+  int l, r;
+  while (cin >> l >> r) {
+    left[l]++;
+    right[r]++;
+  }
+
+  long long result = 0;
+  for (auto iter = left.begin(); iter != left.end(); iter++) {
+    result += (1LL * iter->first * iter->second) * right[iter->first];
+  }
+  cout << result << endl;
+}
